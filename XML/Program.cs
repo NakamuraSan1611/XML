ebd51c4409fb64a8a7b19d4baff684a7ed867d47
СантыбаевХ <@403_2_>
2017-07-20 22:16:47 +0600

    123

--- XML/Program.cs
+++ XML/Program.cs
@@ -8,9 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace XML
 {
+
     class Item
     {
         public string Litle { get; set; }
@@ -43,25 +45,54 @@ namespace XML
 
 
     }
+    class file
+    {
+        public string filename { get; set; }
+        public string filetype { get; set; }
+        public DateTime date { get; set; }
+        public file(string filename, string filetype, DateTime date)
+        {
+            this.filename = filename;
+            this.filetype = filetype;
+            this.date = date;
+        }
+    }
+
     class Program
     {
+        public static object XmkDeclaration { get; private set; }
+
         static void Main(string[] args)
         {
-            string url = "https://habrahabr.ru/rss/interesting/";
-            byte[] data;
-            using (WebClient webClient = new WebClient())
-                data = webClient.DownloadData(url);
 
-            string str = Encoding.GetEncoding("UTF-8").GetString(data);
-            XDocument xd = XDocument.Parse(str);
-            Console.WriteLine(str);
-            string path = @"C: \Users\СантыбаевХ\Documents\visual studio 2015\Projects";
-            string[] files = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
-            List<Order> oreders = new List<Order>();
-            oreders.Add(new Order(1, 1, "brand1", "Article1", "Name1", 1, 1));
-            oreders.Add(new Order(2, 2, "brand2", "Article2", "Name2", 2, 2));
-            oreders.Add(new Order(3, 3, "brand3", "Article3", "Name3", 3, 3));
-            xmlWithPrI(oreders);
+
+            //string url = "https://habrahabr.ru/rss/interesting/";
+            //byte[] data;
+            //using (WebClient webClient = new WebClient())
+            //    data = webClient.DownloadData(url);
+
+            //string str = Encoding.GetEncoding("UTF-8").GetString(data);
+            //XDocument xd = XDocument.Parse(str);
+            //Console.WriteLine(str);
+            string path = @"C:\Users\СантыбаевХ\Desktop\books_DB";
+            string[] files = Directory.GetFiles(path);
+            List<file> fileList = new List<file>();
+            foreach (var item in files)
+            {
+                string name = item.Substring(item.LastIndexOf('\\'), (item.LastIndexOf('.') - 1 - item.LastIndexOf('\\')));
+                string type = item.Substring(item.LastIndexOf('.'), item.Length - item.LastIndexOf('.'));
+                DateTime date = File.GetCreationTime(item);
+                fileList.Add(new file(name, type, date));
+            }
+            xmlWithPrIFile(fileList);
+
+            //List<Order> oreders = new List<Order>();
+            //oreders.Add(new Order(1, 1, "brand1", "Article1", "Name1", 1, 1));
+            //oreders.Add(new Order(2, 2, "brand2", "Article2", "Name2", 2, 2));
+            //oreders.Add(new Order(3, 3, "brand3", "Article3", "Name3", 3, 3));
+            //xmlWithPrI(oreders);
+            //CreateXmlUsingSysXml(oreders[0]);
+            //readXml();
         }
         static void CreatXml()
         {
@@ -119,6 +150,75 @@ namespace XML
                 }
             }
 
+
+        }
+        static void xmlWithPrIFile(List<file> order)
+        {
+            XDocument xdoc = new XDocument();
+
+            XElement ordersRoot = new XElement("files");
+            XElement xmlOrders = new XElement("file");
+            // XAttribute orderAttr = new XAttribute();
+            MethodInfo[] mInfo = typeof(Order).GetMethods();
+            PropertyInfo[] pInfo = typeof(Order).GetProperties();
+            foreach (var item in order)
+            {
+                foreach (var props in mInfo)
+                {
+                    if (props.CustomAttributes.ToString() == "Count = 0")
+                    {
+                        XElement el = new XElement(props.Name, item.GetType().GetProperty(props.Name).GetValue(item, null));
+                        xmlOrders.Add(el);
+                    }
+                }
+            }
+            foreach (var item in order)
+            {
+                foreach (var props in mInfo)
+                {
+                    if (props.CustomAttributes.ToString() == "Count = 0")
+                    {
+                        XAttribute el = new XAttribute(props.Name, item.GetType().GetProperty(props.Name).GetValue(item, null));
+                        xmlOrders.Add(el);
+                    }
+                }
+            }
+
+
+        }
+        static void CreateXmlUsingSysXml(Order order/*, ValidationType validationType*/)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration declaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(declaration);
+            XmlElement rootElement = doc.CreateElement("orders");
+
+            XmlElement orderElement = doc.CreateElement("order");
+            orderElement.SetAttribute("Id", order.id.ToString());
+
+            XmlElement orderElement_UserId = doc.CreateElement("UserId");
+            orderElement_UserId.InnerText = order.UserId.ToString();
+            orderElement_UserId.SetAttribute("UserId", "123456");
+
+            orderElement.AppendChild(orderElement_UserId);
+            rootElement.AppendChild(orderElement);
+            doc.AppendChild(rootElement);
+            doc.Save("name.xml");
+
+
+        }
+        static void readXml()
+        {
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load("https://news.rambler.ru/rss/science/");
+            XmlSerializer formatter = new XmlSerializer(typeof(rss));
+            using (FileStream fs = new FileStream("rss.xml", FileMode.OpenOrCreate))
+            {
+                rss newrss = (rss)formatter.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine("Канал: {0} --- Версия: {1}", newrss.channel, newrss.version);
+            }
 
         }
     }
---
+++ XML/rssRambler.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML
+{
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class rss
+    {
+
+        private rssChannel channelField;
+
+        private decimal versionField;
+
+        /// <remarks/>
+        public rssChannel channel
+        {
+            get
+            {
+                return this.channelField;
+            }
+            set
+            {
+                this.channelField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal version
+        {
+            get
+            {
+                return this.versionField;
+            }
+            set
+            {
+                this.versionField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rssChannel
+    {
+
+        private string titleField;
+
+        private string linkField;
+
+        private string descriptionField;
+
+        private link link1Field;
+
+        private string languageField;
+
+        private string lastBuildDateField;
+
+        private rssChannelImage imageField;
+
+        private rssChannelItem[] itemField;
+
+        /// <remarks/>
+        public string title
+        {
+            get
+            {
+                return this.titleField;
+            }
+            set
+            {
+                this.titleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string link
+        {
+            get
+            {
+                return this.linkField;
+            }
+            set
+            {
+                this.linkField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string description
+        {
+            get
+            {
+                return this.descriptionField;
+            }
+            set
+            {
+                this.descriptionField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("link", Namespace = "http://www.w3.org/2005/Atom")]
+        public link link1
+        {
+            get
+            {
+                return this.link1Field;
+            }
+            set
+            {
+                this.link1Field = value;
+            }
+        }
+
+        /// <remarks/>
+        public string language
+        {
+            get
+            {
+                return this.languageField;
+            }
+            set
+            {
+                this.languageField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string lastBuildDate
+        {
+            get
+            {
+                return this.lastBuildDateField;
+            }
+            set
+            {
+                this.lastBuildDateField = value;
+            }
+        }
+
+        /// <remarks/>
+        public rssChannelImage image
+        {
+            get
+            {
+                return this.imageField;
+            }
+            set
+            {
+                this.imageField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("item")]
+        public rssChannelItem[] item
+        {
+            get
+            {
+                return this.itemField;
+            }
+            set
+            {
+                this.itemField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.w3.org/2005/Atom")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.w3.org/2005/Atom", IsNullable = false)]
+    public partial class link
+    {
+
+        private string hrefField;
+
+        private string relField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string href
+        {
+            get
+            {
+                return this.hrefField;
+            }
+            set
+            {
+                this.hrefField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string rel
+        {
+            get
+            {
+                return this.relField;
+            }
+            set
+            {
+                this.relField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rssChannelImage
+    {
+
+        private string urlField;
+
+        private string titleField;
+
+        private string linkField;
+
+        /// <remarks/>
+        public string url
+        {
+            get
+            {
+                return this.urlField;
+            }
+            set
+            {
+                this.urlField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string title
+        {
+            get
+            {
+                return this.titleField;
+            }
+            set
+            {
+                this.titleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string link
+        {
+            get
+            {
+                return this.linkField;
+            }
+            set
+            {
+                this.linkField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rssChannelItem
+    {
+
+        private rssChannelItemGuid guidField;
+
+        private string titleField;
+
+        private string linkField;
+
+        private string descriptionField;
+
+        private string creatorField;
+
+        private string pubDateField;
+
+        private string[] categoryField;
+
+        /// <remarks/>
+        public rssChannelItemGuid guid
+        {
+            get
+            {
+                return this.guidField;
+            }
+            set
+            {
+                this.guidField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string title
+        {
+            get
+            {
+                return this.titleField;
+            }
+            set
+            {
+                this.titleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string link
+        {
+            get
+            {
+                return this.linkField;
+            }
+            set
+            {
+                this.linkField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string description
+        {
+            get
+            {
+                return this.descriptionField;
+            }
+            set
+            {
+                this.descriptionField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://purl.org/dc/elements/1.1/")]
+        public string creator
+        {
+            get
+            {
+                return this.creatorField;
+            }
+            set
+            {
+                this.creatorField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string pubDate
+        {
+            get
+            {
+                return this.pubDateField;
+            }
+            set
+            {
+                this.pubDateField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("category")]
+        public string[] category
+        {
+            get
+            {
+                return this.categoryField;
+            }
+            set
+            {
+                this.categoryField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rssChannelItemGuid
+    {
+
+        private bool isPermaLinkField;
+
+        private decimal valueField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public bool isPermaLink
+        {
+            get
+            {
+                return this.isPermaLinkField;
+            }
+            set
+            {
+                this.isPermaLinkField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public decimal Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+
+}
